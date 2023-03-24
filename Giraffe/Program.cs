277f7e839ed7572094fe791530bfb886e96c0558
Mikhail <62525324+Mnotice@users.noreply.github.com>
2023-03-24 08:30:43 +0200

    Update Program.cs

--- Giraffe/Program.cs
+++ Giraffe/Program.cs
@@ -13,12 +13,14 @@ namespace Giraffe
             string characterName = "Tom";
             int characterAge;
             characterAge = 25;
+            string mainCharacter = "Joey";
 
             Console.WriteLine("There once was a man named " + characterName);
             Console.WriteLine("He was "+ characterAge +" years old");
             characterName = "Mike";
             Console.WriteLine("He really liked the name " + characterName);
             Console.WriteLine("But didn't like being " + characterAge);
+            Console.WriteLine("But he never liked " + mainCharacter);
                   
             Console.ReadLine();
         }
